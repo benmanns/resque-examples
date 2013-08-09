98388932d63b2db2056597c8f899821b1a5a3537
Benjamin Manns <benmanns@gmail.com>
2013-08-09 15:54:21 -0400

    Generate Rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ResqueExamples::Application.config.secret_key_base = '9e6ff05dfec6dbba7c379e09e180fdcc2451200c4816aa15b2760ec8d1426b212f23792068fa7c55bb14c3e338db9fa0fb0a0a854ab16cff4de328a6e44684f8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ResqueExamples::Application.config.session_store :cookie_store, key: '_resque-examples_session'
